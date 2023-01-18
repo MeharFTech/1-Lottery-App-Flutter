4450a8e4e9a58451c5e4aed7e669c6b1d5c68513
MeharFTech <MeharFTech>
2023-01-19 03:46:17 +0500

    This is my first Commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Lottery App'),
+        ),
+        body: Column(
+          children: [],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Tap');
+          },
+          child: const Icon(Icons.refresh),
+        ),
+      ),
+    );
+  }
+}
