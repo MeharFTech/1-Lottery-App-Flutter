13a146ccbbaba63ebf97c3164c68b029f7dcf3f9
MeharFTech <MeharFTech>
2023-01-19 05:00:53 +0500

    This is my first Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Random random = Random();
+  int x = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,11 +23,66 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Lottery App'),
         ),
         body: Column(
-          children: [],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(child: Text('Winning lottery number is 4')),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(.2),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: x == 4
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.done_all_sharp,
+                            color: Colors.green,
+                            size: 35,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Congratulations You won the lottery your number is $x\n',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.error,
+                            color: Colors.red,
+                            size: 35,
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Better luck next time your number is $x \n try again',
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            print('Tap');
+            x = random.nextInt(10);
+            print(x);
+            setState(() {});
           },
           child: const Icon(Icons.refresh),
         ),
